1a1d8ace63c23f01e3d66c738fbd3efac9f00e69
ryunosuke <ryunosuke.arima@gmail.com>
2024-05-03 13:50:57 +0900

    [all] update 対応

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -11,8 +11,6 @@ require_once __DIR__ . '/../vendor/ryunosuke/phpunit-extension/inc/bootstrap.php
 
 \ryunosuke\PHPUnit\Actual::generateStub(__DIR__ . '/../src', __DIR__ . '/.stub', 2);
 
-\ryunosuke\PHPUnit\Actual::$functionNamespaces = [];
-
 define('STREAM_WRAPPER_DEBUG', true);
 
 defined('REDIS_URL') or define('REDIS_URL', null);
