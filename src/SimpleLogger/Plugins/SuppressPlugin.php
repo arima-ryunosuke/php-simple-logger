4a21625814d155c1cc93aaf8536a83679957e9de
ryunosuke <ryunosuke.arima@gmail.com>
2024-07-20 15:51:30 +0900

    [feature] 同一ログを一定時間抑制するプラグイン

---
+++ src/SimpleLogger/Plugins/SuppressPlugin.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace ryunosuke\SimpleLogger\Plugins;
+
+use Closure;
+use ryunosuke\SimpleLogger\Item\Log;
+use Throwable;
+
+class SuppressPlugin extends AbstractPlugin
+{
+    private int     $aggregationTime;
+    private string  $rememberLocation;
+    private Closure $keyProvider;
+
+    private array $suppressions;
+
+    public function __construct(float $aggregationTime, ?string $rememberLocation = null, ?Closure $keyProvider = null)
+    {
+        $this->aggregationTime  = $aggregationTime;
+        $this->rememberLocation = $rememberLocation ?? sys_get_temp_dir() . '/logger-suppressions.txt';
+        $this->keyProvider      = $keyProvider ?? static function (Log $log) {
+            return sprintf('%s:%s(%s)', $log->level, $log->message, json_encode($log->context));
+        };
+
+        try {
+            $this->suppressions = (include $this->rememberLocation) ?: [];
+        }
+        catch (Throwable) {
+            $this->suppressions = [];
+        }
+    }
+
+    public function __destruct()
+    {
+        $aggregations = array_filter($this->suppressions, fn($time) => (microtime(true) - $time) < $this->aggregationTime);
+
+        file_put_contents($this->rememberLocation, "<?php return " . var_export($aggregations, true) . ";", LOCK_EX);
+    }
+
+    public function apply(Log $log): ?Log
+    {
+        $aggkey = ($this->keyProvider)($log);
+        if ($aggkey !== null) {
+            if ((microtime(true) - ($this->suppressions[$aggkey] ?? 0)) < $this->aggregationTime) {
+                return null;
+            }
+            $this->suppressions[$aggkey] = microtime(true);
+        }
+
+        return $log;
+    }
+}
---
+++ tests/Test/SimpleLogger/Plugins/SuppressPluginTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace ryunosuke\Test\SimpleLogger\Plugins;
+
+use ryunosuke\SimpleLogger\Item\Log;
+use ryunosuke\SimpleLogger\Plugins\SuppressPlugin;
+use ryunosuke\Test\AbstractTestCase;
+
+class SuppressPluginTest extends AbstractTestCase
+{
+    function test_apply()
+    {
+        $plugin = new SuppressPlugin(1, sys_get_temp_dir() . '/agg1.txt');
+
+        that($plugin)->apply(new Log('debug', 'message', ['hoge' => '1']))->isNotNull();
+        that($plugin)->apply(new Log('debug', 'message', ['hoge' => '1']))->isNull();
+        that($plugin)->apply(new Log('debug', 'message', ['hoge' => '2']))->isNotNull();
+        that($plugin)->apply(new Log('debug', 'message', ['hoge' => '2']))->isNull();
+
+        that($plugin)->apply(new Log('debug', 'message-{hoge}', ['hoge' => '1']))->isNotNull();
+        that($plugin)->apply(new Log('debug', 'message-{hoge}', ['hoge' => '1']))->isNull();
+        that($plugin)->apply(new Log('debug', 'message-{hoge}', ['hoge' => '2']))->isNotNull();
+        that($plugin)->apply(new Log('debug', 'message-{hoge}', ['hoge' => '2']))->isNull();
+
+        sleep(1);
+        that($plugin)->apply(new Log('debug', 'message-{hoge}', ['hoge' => '1']))->isNotNull();
+    }
+
+    function test_lifecycle()
+    {
+        $log = sys_get_temp_dir() . '/agg2.txt';
+        @unlink($log);
+        $plugin = new SuppressPlugin(1, $log);
+
+        that($plugin)->apply(new Log('debug', 'message1', []))->isNotNull();
+        sleep(1);
+        that($plugin)->apply(new Log('debug', 'message2', []))->isNotNull();
+
+        unset($plugin);
+        gc_collect_cycles();
+
+        that($log)->fileNotContains('message1');
+        that($log)->fileContains('message2');
+    }
+}
